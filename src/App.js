8b92901662dc1d0c41e00b76ddde23bf57147b59
Daniel Carvalho <danielbpc2@Hotmail.com>
2023-10-17 15:59:33 -0300

    feat: adding, removing and listing api repositories

--- src/App.js
+++ src/App.js
@@ -1,27 +1,49 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import {
+  getRepositories,
+  addRepository,
+  removeRepository,
+} from "./services/api";
+import RepositoryList from "./components/RepositoryList";
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    getRepositories().then((response) => {
+      setRepositories(response);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const repo = {
+      url: "https://github.com/rocketseat-education/bootcamp-gostack-desafios/blob/master/desafio-conceitos-reactjs/README.md",
+      title: `Repo do desafio ReactJS`,
+      techs: ["React", "Node.js"],
+    };
+    const response = await addRepository(repo);
+
+    setRepositories([...repositories, response]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    const responseStatus = await removeRepository(id);
+
+    if (responseStatus === 204) {
+      const updatedRepositories = repositories.filter(
+        (repository) => repository.id !== id
+      );
+      setRepositories(updatedRepositories);
+    }
   }
 
   return (
     <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
-      </ul>
+      <RepositoryList
+        repositories={repositories}
+        handleRemoveRepository={handleRemoveRepository}
+      />
 
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
--- src/services/api.js
+++ src/services/api.js
@@ -4,4 +4,19 @@ const api = axios.create({
   baseURL: "http://localhost:3333",
 });
 
+export const getRepositories = async () => {
+  const response = await api.get("/repositories");
+  return response.data;
+};
+
+export const addRepository = async (repository) => {
+  const response = await api.post("/repositories", repository);
+  return response.data;
+};
+
+export const removeRepository = async (id) => {
+  const response = await api.delete(`/repositories/${id}`);
+  return response.status;
+};
+
 export default api;
